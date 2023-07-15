e99f0556bf7f041948349bd994854766b01f7f7a
unknown <ebojimichael@gmail.com>
2023-07-15 17:23:49 +0100

    My first Commit

---
+++ my_first_app/lib/home_page.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10.0,
+                vertical: 5.0,
+              ),
+              child: Wrap(
+                spacing: 10,
+                children: [
+                ElevatedButton(
+                  onPressed: (){}, 
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.redAccent,
+                    foregroundColor: Colors.white,
+                    shape: const StadiumBorder(),
+                  ),
+                  child: const Text('Money'),
+                  ),
+                    ElevatedButton(
+                  onPressed: (){}, 
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.redAccent,
+                    foregroundColor: Colors.white,
+                    shape: const StadiumBorder(),
+                  ),
+                  child: const Text('Bitcoin'),
+                  ),
+                    ElevatedButton(
+                  onPressed: (){}, 
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.redAccent,
+                    foregroundColor: Colors.white,
+                    shape: const StadiumBorder(),
+                  ),
+                  child: const Text('Stock Market'),
+                  ),
+                   ElevatedButton(
+                  onPressed: (){}, 
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.redAccent,
+                    foregroundColor: Colors.white,
+                    shape: const StadiumBorder(),
+                  ),
+                  child: const Text('House Market'),
+                  ),
+                   ElevatedButton(
+                  onPressed: (){}, 
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.redAccent,
+                    foregroundColor: Colors.white,
+                    shape: const StadiumBorder(),
+                  ),
+                  child: const Text('Diamond hands'),
+                  ),
+              ],
+              ),
+            ),
+            GestureDetector(
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context){
+                  return Container();  
+                }));
+              },
+              child: Container(  
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 10.0,
+                  vertical: 5.0,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white, 
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black12,
+                      spreadRadius: 2,
+                      offset: Offset(2, 2)
+                    )
+                  ]
+                  ),
+                child: Column(
+                  children: [
+                    Image.asset("images/money.png"),
+                   const ListTile(
+                      title: Text("How to get Rich"),
+                      trailing: Icon(Icons.arrow_forward_ios_rounded),
+                    )
+                ]),
+              ),
+            ),
+            Container(  
+              width: double.infinity,
+              margin: const EdgeInsets.symmetric(
+                horizontal: 10.0,
+                vertical: 5.0,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white, 
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black12,
+                    spreadRadius: 2,
+                    offset: Offset(2, 2)
+                  )
+                ]
+                ),
+              child: Column(
+                children: [
+                  Image.asset("images/money.png"),
+                 const ListTile(
+                    title: Text("How to get Rich"),
+                    trailing: Icon(Icons.arrow_forward_ios_rounded),
+                  )
+              ]),
+            ),
+            Container(  
+              width: double.infinity,
+              margin: const EdgeInsets.symmetric(
+                horizontal: 10.0,
+                vertical: 5.0,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white, 
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black12,
+                    spreadRadius: 2,
+                    offset: Offset(2, 2)
+                  )
+                ]
+                ),
+              child: Column(
+                children: [
+                  Image.asset("images/money.png"),
+                 const ListTile(
+                    title: Text("How to get Rich"),
+                    trailing: Icon(Icons.arrow_forward_ios_rounded),
+                  )
+              ]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ my_first_app/lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:my_first_app/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        //colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        //useMaterial3: true,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Welcome",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 55,
+                  fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              "To Map Blog",
+              style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Image.asset("images/login.jpg"),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context)
+                .push(MaterialPageRoute(builder: (BuildContext context){
+                 return const HomePage();
+                },
+                )
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.red,
+                minimumSize:const Size(300, 40),
+              ),
+              child: const Text("Login"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:Colors.red,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(300, 40) ),
+                child: const Text("Register"))
+          ],
+        ),
+      ),
+    );
+  }
+}
